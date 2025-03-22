2e60be9d06a2fbbd18f7b51e4d92a8ea087055d6
Mads Andreasen <mads@andreasen.cc>
2025-03-22 15:10:01 +0000

    ctrl-c aborts the application

--- src/can_eth/__main__.py
+++ src/can_eth/__main__.py
@@ -1,8 +1,12 @@
 import argparse
+import logging
 import can
 import socket
 import asyncio
+import signal
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class Forwarder:
     def __init__(self, can_interface, ip_address, port):
@@ -15,15 +19,36 @@ class Forwarder:
         bus = can.interface.Bus(channel=self.can_interface, interface='socketcan')
         loop = asyncio.get_event_loop()
         while True:
-            message =   await loop.run_in_executor(None, bus.recv)
-            if message:
-                self.sock.sendto(message.data, (self.ip_address, self.port))
+            try:
+                message = await loop.run_in_executor(None, bus.recv)
+                if message:
+                    logger.debug("Received message: %s", message)
+                    self.sock.sendto(message.data, (self.ip_address, self.port))
+            except asyncio.CancelledError:
+                bus.shutdown()
+                break
+
+async def handle_sigint():
+    loop = asyncio.get_event_loop()
+    stop_event = asyncio.Event()
+
+    def signal_handler():
+        stop_event.set()
+
+    loop.add_signal_handler(signal.SIGINT, signal_handler)
+    await stop_event.wait()
 
 
 async def main():
     args = parse_args()
     forwarder = Forwarder(args.can_interface, args.i, args.p)
-    await forwarder.forward()
+    forward_task = asyncio.create_task(forwarder.forward())
+    sigint_task = asyncio.create_task(handle_sigint())
+
+    await asyncio.wait([forward_task, sigint_task], return_when=asyncio.FIRST_COMPLETED)
+
+    forward_task.cancel()
+    await forward_task
 
 
 def parse_args():
